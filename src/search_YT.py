06cb1e7d82b311c02b2fbd9b0fb003c8c34c24e1
Kai-bo Chang <75492436+billy122333@users.noreply.github.com>
2022-09-30 18:49:05 +0800

    Update search_YT.py

--- src/search_YT.py
+++ src/search_YT.py
@@ -15,7 +15,7 @@ import kkbox
 GMAIL = 'YOUR GMAIL'
 PASSWORD = 'YOUR PASSWORD'
 conn = mysql.connector.connect(host='127.0.0.1', port='3306', user='root',
-                               password='Qrs133666!', database='rank_list', charset='utf8')
+                               password='###', database='rank_list', charset='utf8')
 cur = conn.cursor()
 
 if __name__ == '__main__':
